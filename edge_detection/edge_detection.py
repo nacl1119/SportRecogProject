e8b0e39ac01f3b11e6c60957d33b1a260637c4ef
Gangyeol Kim <kkyy3402@naver.com>
2020-09-07 14:57:35 +0000

    기본 세팅 변경

--- edge_detection/edge_detection.py
+++ edge_detection/edge_detection.py
@@ -15,7 +15,7 @@ def edge_detection(color_img):
     if M > N: # 행이 열보다 길면 오른쪽으로 회전
         color_img = cv2.rotate(color_img, cv2.ROTATE_90_CLOCKWISE) 
     
-    cv2.imshow('color', color_img)
+    #cv2.imshow('color', color_img)
     
     
     '엣지 검출'
@@ -24,25 +24,25 @@ def edge_detection(color_img):
     edge_y = cv2.Sobel(color_img, cv2.CV_64F, 0, 1, ksize=3)
     edge_y = cv2.convertScaleAbs(edge_y)
     edge = cv2.addWeighted(edge_x, 0.5, edge_y, 0.5, 0);
-    cv2.imshow('raw_edge', edge)
+    #cv2.imshow('raw_edge', edge)
     
     
     '엣지에서 노이즈 제거'
     edge = cv2.fastNlMeansDenoisingColored(edge, None, 20, 20, 15, 25) 
-    cv2.imshow('denoised_edge', edge)
+    #cv2.imshow('denoised_edge', edge)
     
     
     '남은 엣지 날카롭게'
     sharpening = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
     edge = cv2.filter2D(edge, -1, sharpening)
-    cv2.imshow('sharpened_denoised_edge', edge)
+    #cv2.imshow('sharpened_denoised_edge', edge)
     
     '컬러 -> 흑백'
     edge = cv2.cvtColor(edge, cv2.COLOR_BGR2GRAY)
     
     '다시 한번 노이즈 제거'
     edge[edge <= 30] = edge[edge <= 30]*0.1 
-    cv2.imshow('denoised_sharpened_denoised_edge', edge)
+    #cv2.imshow('denoised_sharpened_denoised_edge', edge)
     
     '흑백 전환'
     edge = 255-edge
@@ -52,9 +52,9 @@ def edge_detection(color_img):
     terminate_time = timeit.default_timer() # 시간 측정 종료
     print("{}초 소요".format(terminate_time - start_time))
     
-    cv2.imshow('final edge', edge)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow('final edge', edge)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
     return edge
     
